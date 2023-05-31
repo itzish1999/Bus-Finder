0f31ab9e5e5755e0a56e5cefd661b25e5d3499ad
itzish1999 <ismaeel.butt19@gmail.com>
2023-05-31 16:27:15 -0400

    feat(Controller): Created a controller to fetch bus stop data

---
+++ server/src/controllers/busStopController.js
@@ -0,0 +1,28 @@
+const axios = require('axios');
+const { apiKey } = require('../config/config');
+
+const getBusStop = (req, res) => {
+    const coords = {
+        longitude: req.body.longitude,
+        latitude: req.body.latitude,
+    };
+
+    console.log('coords', coords)
+
+    const route = `http://bustime.mta.info/api/where/stops-for-location.json?lat=${coords.latitude}&lon=${coords.longitude}&latSpan=0.005&lonSpan=0.005&key=${apiKey}`;
+
+    axios.get(route)
+        .then(response => {
+            const responseData = response.data
+            console.log('response : ', responseData);
+            res.json(responseData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: 'Failed to fetch bus stop data!' })
+        });
+}
+
+module.exports = {
+    getBusStop,
+}
