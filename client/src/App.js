d088f93a25ba0a0e853ce3244aef45dbe02778a3
itzish1999 <ismaeel.butt19@gmail.com>
2023-05-31 14:53:16 -0400

    feat(React App): created react app, sent coordinates to backend

---
+++ client/src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+function App() {
+  const [location, setLocation] = useState({});
+
+  useEffect(() => {
+    getLocation();
+    if (location?.latitude && location?.longitude) {
+      axios.post('http://localhost:8000/api/location', location)
+        .then(res => {
+          console.log('Location sent:', res.data);
+        })
+        .catch(error => {
+          console.error('Error sending location:', error);
+        });
+    }
+  }, [location.latitude, location.longitude]);
+
+  const getLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log('position', position);
+      const { latitude, longitude } = position.coords;
+      console.log("latitude::: ", latitude)
+      console.log("longitude::: ", longitude)
+      setLocation({ latitude, longitude });
+    });
+  }
+
+  return (
+    <div className="App">
+      <p>BUS APP</p>
+    </div>
+  );
+}
+
+export default App;
